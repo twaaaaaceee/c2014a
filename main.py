9b4ba1b58e9d7b633f1aa0a90ec24bf713e2207b
Admin <mrsharkyt69@gmail.com>
2024-01-20 10:36:38 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,75 +1,40 @@
-import random
+import requests
 
+pars_res_list=[]
+response=requests.get("https://bank.gov.ua/ua/markets/exchangerate")
+response_text=response.text
+response_parse=response_text.split("<span>")
+for parse_elem_2 in response_parse:
+    if parse_elem_2.startswith("$"):
+        for parse_elem_2 in parse_elem_2.split("</span>"):
+            if parse_elem_2.startswith("$") and parse_elem_2[1].isdigit():
+                pars_res_list.append(parse_elem_2)
 
-class Student:
-    def __init__(self,name):
-        self.name=name
-        self.gladness=50
-        self.progress=0
-        self.money=100
-        self.alive=True
+AUD=pars_res_list[0]
+print(f"AUD - {AUD}")
 
+AZN=pars_res_list[1]
+print(f"AZN - {AZN}")
 
-    def to_working(self):
-        print("time go to job")
-        self.progress+=0.5
-        self.gladness-=1.5
-        self.money+=50
-    def to_study(self):
-            print("time to study")
-            self.progress+=0.12
-            self.gladness-=3
-            self.money-=10
-    def to_sleep(self):
-            print("i will sleep")
-            self.progress+=3
-            self.gladness+=2
-            self.money+=0
-    def to_chill(self):
-            print("Rest time")
-            self.progress-=0.1
-            self.gladness+=5
-            self.money-=50
-    def is_alive(self):
-        if self.progress<=0.5:
-            print("Cast out")
-            self.alive=False
-        elif self.gladness>=0:
-            print("Depresion . .")
-            self.alive=False
-        elif self.progress>5:
-            print("Passed externaly")
-            self.alive=False
-        elif self.money>=0:
-            print("will be kicked from the house")
-            self.alive=False
-    def end_of_day(self):
-        print(f"Gladness - {self.gladness}")
-        print(f"Progress - {round(self.progress,2)}")
+BYN=pars_res_list[2]
+print(f"BYN - {BYN}")
 
-    def live(self,day):
-        day="Day" + str(day) + "of" + self.name + "life"
-        print(f"{day:=^50}")
-        live.cube=random.randint(1,3)
-        if live.cube==1:
-            self.to.study()
-        elif live_cube==2:
-            self.to.sleep()
-        elif live_cube==3:
-            self.to.chill()
-        elif live_cube==4:
-            self.to.working()
-        self.end_of_day()
-        self.is_alive()
-nick=Student(name="Nick")
-for day in range(365):
-    if nick.alive==False:
-        break
-    nick.live(day)
+BGN=pars_res_list[3]
+print(f"BGN - {BGN}")
 
+KRW=pars_res_list[4]
+print(f"KRW - {KRW}")
 
+HKD=pars_res_list[5]
+print(f"HKD - {HKD}")
 
+DKK=pars_res_list[6]
+print(f"DKK - {DKK}")
 
+USD=pars_res_list[7]
+print(f"USD - {USD}")
 
+EUR=pars_res_list[8]
+print(f"Avalanche - {EUR}")
 
 
